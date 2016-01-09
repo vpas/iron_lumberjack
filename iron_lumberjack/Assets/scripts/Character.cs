ce879bd5255340f9e7d1fa6c582fa68298e4de9c
Skel <mail@mail.com>
2016-01-09 15:36:36 -0800

    cosmetics

--- iron_lumberjack/Assets/scripts/Character.cs
+++ iron_lumberjack/Assets/scripts/Character.cs
@@ -3,8 +3,6 @@ using System.Collections;
 using UnityEngine.Networking;
 
 public class Character : NetworkBehaviour {
-	public static string[] characterNames = new string[] { "girl", "woodman", "Ninzya" };
-
 	private static ArrayList characters = new ArrayList();
 //	public int curTextureIndex = 0;
 //	public float lastTextureChangeTime = 0;
@@ -18,61 +16,36 @@ public class Character : NetworkBehaviour {
 
 	public Material characterMaterial;
 	private Texture[] attackAnimationTextures;
-	private Texture[] moveAnimationTextures;
 
 	private bool inAttackingState = false;
 
-	private bool isMoving = false;
-
 	[SyncVar]
-	private string charName;
-
-	private string nonSyncedCharName;
+	private int playerId;
 
 	// Use this for initialization
 	void Start () {
-		this.charName = characterNames [Random.Range (0, characterNames.Length)];
-
-		attackAnimationTextures = loadTextures (charName + "Attack");
-		moveAnimationTextures = loadTextures (charName + "Walk");
+		Object[] texturesAsObj = Resources.LoadAll (attackAnimationDir);
+		Debug.Log ("texturesAsObj size: " + texturesAsObj.GetLength (0));
+		attackAnimationTextures = new Texture[texturesAsObj.GetLength(0)];
+		for (int i = 0; i < texturesAsObj.GetLength(0); i++) {
+			attackAnimationTextures [i] = (Texture)texturesAsObj [i];
+		}
 
 		var clonedMaterial = new Material (Shader.Find("Standard"));
 		clonedMaterial.CopyPropertiesFromMaterial (characterMaterial);
 		characterMaterial = clonedMaterial;
 		transform.GetChild(1).GetComponent<MeshRenderer> ().material = clonedMaterial;
-		characterMaterial.SetTexture ("_MainTex", attackAnimationTextures [0]);
 
 		characters.Add (this);
 		Spawn ();
 	}
 
-
-	void UpdateTextures() {
-		attackAnimationTextures = loadTextures (charName + "Attack");
-		moveAnimationTextures = loadTextures (charName + "Walk");
-
-		var clonedMaterial = new Material (Shader.Find("Standard"));
-		clonedMaterial.CopyPropertiesFromMaterial (characterMaterial);
-		characterMaterial = clonedMaterial;
-		transform.GetChild(1).GetComponent<MeshRenderer> ().material = clonedMaterial;
-		characterMaterial.SetTexture ("_MainTex", attackAnimationTextures [0]);
-	}
-
 	float lastLogTime = 0;
-
-	Texture[] loadTextures(string path) {
-		Object[] texturesAsObj = Resources.LoadAll (path);
-		Texture[] textures = new Texture[texturesAsObj.GetLength(0)];
-		for (int i = 0; i < texturesAsObj.GetLength(0); i++) {
-			textures [i] = (Texture)texturesAsObj [i];
-		}
-		return textures;
-	}
 
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (Time.time - lastLogTime > 5) {
-			DebugConsole.Log ("Character netId: " + netId + " coords: " + transform.position + " mat: " + characterMaterial.GetInstanceID() + " health: " + health);
+			DebugConsole.Log ("Character netId: " + netId + " coords: " + transform.position + " mat: " + characterMaterial.GetInstanceID());
 			lastLogTime = Time.time;
 		}
 
@@ -80,35 +53,21 @@ public class Character : NetworkBehaviour {
 			return;
 		}
 
-		bool wasMoving = isMoving;
-		isMoving = false;
 		var v2d = new Vector2 (0, 0);
 		if (Input.GetKey (KeyCode.W)) {
 			v2d += Vector2.up;
-			isMoving = true;
 		}
 		if (Input.GetKey (KeyCode.S)) {
 			v2d += Vector2.down;
-			isMoving = true;
 		}
 		if (Input.GetKey (KeyCode.A)) {
 			v2d += Vector2.left;
-			isMoving = true;
 		}
 		if (Input.GetKey (KeyCode.D)) {
 			v2d += Vector2.right;
-			isMoving = true;
 		}
 		SetVelocity (v2d);
 		transform.position = new Vector3 (transform.position.x, 0.5f, transform.position.z);
-
-		if (!inAttackingState) {
-			if (isMoving && !wasMoving) {
-				CmdStartWalkAnimation (netId);
-			} else if (wasMoving && !isMoving) {
-				CmdStopWalkAnimation (netId);
-			}
-		}
 	}
 
 	void SetVelocity(Vector2 v2d) {
@@ -121,13 +80,7 @@ public class Character : NetworkBehaviour {
 //	}
 
 	void Update() {
-		if (nonSyncedCharName == null || !nonSyncedCharName.Equals (charName)) {
-			nonSyncedCharName = charName;
-			UpdateTextures ();
-		}
-
 		if (!isLocalPlayer) {
-			transform.GetChild (0).GetComponent<Light> ().intensity = 0;
 			return;
 		}
 
@@ -159,7 +112,10 @@ public class Character : NetworkBehaviour {
 
 	void Die() {
 		Spawn ();
+		GameObject.Find ("MusicManager").GetComponent<MusicController> ().playDeathSound();
 	}
+
+
 
 	void Spawn() {
 		GameObject[] respawnPoints = GameObject.FindGameObjectsWithTag ("Respawn");
@@ -173,13 +129,33 @@ public class Character : NetworkBehaviour {
 		}
 	}
 
+	IEnumerator ShutdownFavn(Transform favn)
+	{
+		yield return new WaitForSeconds (2f);
+
+		for (int i = 0; i < favn.childCount; ++i) {
+			favn.GetChild (i).gameObject.SetActive (false);
+
+		}
+	}
+
 	void OnTriggerEnter(Collider other) {
 		Debug.Log ("OnTriggerEnter: tag: " + other.tag);
 		if (other.tag.Equals("Spikes")) {
 			Die();
 		}
 
-		if (other.tag.Equals("Weapon") && other.GetComponentInParent<Character>().inAttackingState) {
+		if (other.tag.Equals ("Favn")) {
+			for (int i = 0; i < other.transform.childCount; ++i) {
+				other.transform.GetChild (i).gameObject.SetActive (true);
+
+			}
+			StartCoroutine (ShutdownFavn (other.transform));
+
+			Invoke ("Die", 0.6f /*timeToDeath*/);
+		}
+
+		if (other.tag.Equals("Weapon")) {
 			Hit (other.GetComponent<IWeapon> ().GetDamage ());
 		}
 	}
@@ -214,67 +190,7 @@ public class Character : NetworkBehaviour {
 			0.5f,
 			new System.Action (delegate {
 				inAttackingState = false;
-			}),
-			-1f,
-			false
+			})
 		);
-	}
-
-	[Command]
-	void CmdStartWalkAnimation(NetworkInstanceId netId) {
-		Debug.Log ("CmdStartWalkAnimation netId: " + netId);
-		foreach (Character character in characters) {
-			if (character.netId.Equals(netId)) {
-				character.StartWalkAnimation ();
-			}
-		}
-		RpcStartWalkAnimation (netId);
-	}
-
-	[ClientRpc]
-	void RpcStartWalkAnimation(NetworkInstanceId netId) {
-		Debug.Log ("RpcStartWalkAnimation netId: " + netId);
-		foreach (Character character in characters) {
-			if (character.netId.Equals(netId)) {
-				character.StartWalkAnimation ();
-			}
-		}
-	}
-
-	void StartWalkAnimation() {
-		GetComponent<AnimationPlayer> ().StartAnimation (
-			attackAnimationTextures,
-			characterMaterial,
-			100000f,
-			new System.Action (delegate {
-			}),
-			1.0f / 24f,
-			true
-		);
-	}
-
-	[Command]
-	void CmdStopWalkAnimation(NetworkInstanceId netId) {
-		Debug.Log ("CmdStopWalkAnimation netId: " + netId);
-		foreach (Character character in characters) {
-			if (character.netId.Equals(netId)) {
-				character.StopWalkAnimation ();
-			}
-		}
-		RpcStopWalkAnimation (netId);
-	}
-
-	[ClientRpc]
-	void RpcStopWalkAnimation(NetworkInstanceId netId) {
-		Debug.Log ("RpcStopWalkAnimation netId: " + netId);
-		foreach (Character character in characters) {
-			if (character.netId.Equals(netId)) {
-				character.StopWalkAnimation ();
-			}
-		}
-	}
-
-	void StopWalkAnimation() {
-		GetComponent<AnimationPlayer> ().StopAnimation ();
 	}
 }
--- iron_lumberjack/Assets/scripts/MusicController.cs
+++ iron_lumberjack/Assets/scripts/MusicController.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class MusicController : MonoBehaviour {
 	public AudioSource musicAudioSource;
+	public AudioSource deathAudioSource;
 
 	Object[] myMusic;
+	Object[] deathSounds;
 
 	void Awake () {
 		myMusic = Resources.LoadAll("Audio/Music", typeof(AudioClip));
+		deathSounds = Resources.LoadAll("Audio/Death", typeof(AudioClip));
 		Debug.Log (System.Environment.CurrentDirectory);
 		playRandomMusic();
 	}
@@ -26,5 +29,10 @@ public class MusicController : MonoBehaviour {
 	void playRandomMusic() {
 		musicAudioSource.clip = myMusic[Random.Range(0, myMusic.Length)] as AudioClip;
 		musicAudioSource.Play();
+	}
+
+	public void playDeathSound() {
+		deathAudioSource.clip = deathSounds[Random.Range(0, deathSounds.Length)] as AudioClip;
+		deathAudioSource.Play();
 	}
 }
