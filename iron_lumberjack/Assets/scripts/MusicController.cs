385b8adf53984d7b03fda3b3aa6d93a54276af11
Skel <mail@mail.com>
2016-01-09 11:06:18 -0800

    Bgrns msc

---
+++ iron_lumberjack/Assets/scripts/MusicController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour {
+	public AudioSource musicAudioSource;
+
+	Object[] myMusic;
+
+	void Awake () {
+		myMusic = Resources.LoadAll("Audio/Music", typeof(AudioClip));
+		Debug.Log (System.Environment.CurrentDirectory);
+		playRandomMusic();
+	}
+
+	void Start () {
+		musicAudioSource.Play(); 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!musicAudioSource.isPlaying) {
+			playRandomMusic();
+		}
+	}
+
+	void playRandomMusic() {
+		musicAudioSource.clip = myMusic[Random.Range(0, myMusic.Length)] as AudioClip;
+		musicAudioSource.Play();
+	}
+}
